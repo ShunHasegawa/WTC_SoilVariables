9f28ef5da0eb1004ab84c38c01c1d8ac62f8f103
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-26 02:15:15 +1000

    weird valued in some the variables

--- R/Analysis.R
+++ R/Analysis.R
@@ -21,18 +21,21 @@ allsoils <- downloadTOA5("WTC.._Table1",cachefile="Data/hievdata/tmp.RData",
                          topath="Data/hievdata/row_data", maxnfiles = 300)
 pr_allsoils <- PrcsDat(allsoils)
 
+
+summary(pr_allsoils)
+# there are some weird valued especially in Temp10, Temp100
+pr_allsoils$Temp10[which(pr_allsoils$Temp10 < 2 | pr_allsoils$Temp10 > 40)] <- NA
+pr_allsoils$Temp100[which(pr_allsoils$Temp100 < 2 | pr_allsoils$Temp100 > 40)] <- NA
+pr_allsoils$SoilTemp10_1[which(pr_allsoils$SoilTemp10_1 < 5)] <- NA
+
 # mean of SoilTemp10
 soilRowMean <- cbind(pr_allsoils[ ,-grep("SoilTemp10", names(pr_allsoils))], 
                      'SoilTemp10' = rowMeans(pr_allsoils[ ,grep("SoilTemp10", names(pr_allsoils))], na.rm = TRUE))
 
-head(soilRowMean)
-
-soilMlt <- melt(soilRowMean, id = c("DateTime", "Date", "RECORD", "Date", "Source", "Chamber", "temp"))
-
-soilMlt <- melt(te, id = c("DateTime", "Date", "RECORD", "Date", "Source", "Chamber", "temp"))
-
 # Daily mean
 DayChmMean <- ddply(soilRowMean, .(Date, Chamber, temp), colSmryDF)
+summary(DayChmMean)
+
 save(DayChmMean, file = "Output/Data/WTC_soilMoistTemp_Chamber_DailyMean.RData")
 
 DayTrtMean <- ddply(DayChmMean, .(Date, temp), colMeanDF)
