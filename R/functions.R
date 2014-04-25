dad1361b80f72c545a521b7e690c6247f0ffa988
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-26 01:05:05 +1000

    min and max temp

--- R/functions.R
+++ R/functions.R
@@ -15,12 +15,21 @@ PrcsDat <- function(data){
   return(data)
 }
 
-######################################
-# Column mean for required variables #
-######################################
-colMeanDF <- function(data){
+#################################################################
+# Column mean and minimum & maximum temp for required variables #
+#################################################################
+colSmryDF <- function(data){
   a <- data[, c("SoilVW_5_25", "SoilVW_30_50", "SoilVW_HL", 
                 "Temp5", "Temp10", "Temp20", "Temp30", "Temp50", "Temp100", 
                 "SoilTemp10")]
-  colMeans(a, na.rm = TRUE)
+  means <- colMeans(a, na.rm = TRUE)
+  
+  TmpMin <- apply(a[, grep("Temp", names(a))], 2, min, na.rm = TRUE)
+  names(TmpMin) <- paste("min", names(TmpMin), sep = "_")
+  
+  TmpMax <- apply(a[, grep("Temp", names(a))], 2, max, na.rm = TRUE)
+  names(TmpMax) <- paste("max", names(TmpMin), sep = "_")
+  c(means, TmpMin, TmpMax)
 }
+
+
