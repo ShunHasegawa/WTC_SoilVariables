4327df8059077cb53a765d2e9a2c74cec6565e68
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-26 17:33:08 +1000

    xaxis lable font size are adjusted

--- R/Figs.R
+++ R/Figs.R
@@ -33,5 +33,6 @@ ggsave(filename= "Output//Figs/WTC_SoilTemp10cm.pdf", plot = ToTemp, width = 6, 
 # stratified temperature
 TempDifDep <- PltTemp(data = soilTrtSmry[grep("^Temp", soilTrtSmry$variable), ],
                       ylab = expression(Soil~temperature~(~degree~C)), size = 0.1) +
-  facet_wrap(~variable, ncol = 2)   
-ggsave(filename= "Output//Figs/WTC_SoilTmpDepths.pdf", plot = TempDifDep, width = 6, height = 8)  
+  facet_wrap(~variable, ncol = 2) +
+  theme(axis.text.x = element_text(size = 6))
+ggsave(filename= "Output//Figs/WTC_SoilTmpDepths.pdf", plot = TempDifDep, width = 6, height = 6)  
