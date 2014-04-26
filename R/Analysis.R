f7b7e3a05fcc7f942d764ac4292e4e0246e6a215
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-26 16:59:55 +1000

    fig section is sourced

--- R/Analysis.R
+++ R/Analysis.R
@@ -88,50 +88,5 @@ save(soilTrtSmry, file = "Output/Data/WTC_soilMoistTemp_TempTrt_DailySummary.RDa
 ########
 # Figs #
 ########
-unique(soilTrtSmry$variable)
-theme_set(theme_bw())
-
-############
-# Moisture #
-############
-# surface moisture
-TopMoist <- PltMoist(data = subset(soilTrtSmry, variable == "SoilVW_5_25"),
-                     ylab = "Soil moisture at 5-25 cm\n(% of volumetric water content)")
-ggsave(filename= "Output//Figs/WTC_SoilMoisture5_25cm.pdf", plot = TopMoist, width = 6, height = 3)
-
-# moisture at different depths
-ylabs <- list(
-  'SoilVW_5_25' = "5-25 cm",
-  'SoilVW_30_50' = "30-50 cm",
-  'SoilVW_HL' = "HL"
-)
-MoistDifDep <- PltMoist(data = soilTrtSmry[grep("SoilVW", soilTrtSmry$variable), ],
-                         ylab = "Soil moisture\n(% of volumetric water content)") + 
-  facet_grid(variable ~., labeller= ylab_label)
-
-ggsave(filename= "Output//Figs/WTC_SoilMoistureDepths.pdf", plot = MoistDifDep, width = 6, height = 6)
-
-
-###############
-# Temperature #
-###############
-# surface temperature
-ToTemp <- PltTemp(data = subset(soilTrtSmry, variable == "SoilTemp10"),
-                                ylab = expression(Soil~temperature~at~10~cm~(~degree~C)))
-ggsave(filename= "Output//Figs/WTC_SoilTemp10cm.pdf", plot = ToTemp, width = 6, height = 3)
-
-# stratified temperature
-ylabs <- list(
-  'Temp5' = "5 cm",
-  'Temp10' = "10 cm",
-  'Temp20' = "20 cm",
-  'Temp30' = "30 cm",
-  'Temp50' = "50 cm",
-  'Temp100' = "100 cm"
-  )
-
-TempDifDep <- PltTemp(data = soilTrtSmry[grep("^Temp", soilTrtSmry$variable), ],
-                      ylab = expression(Soil~temperature~(~degree~C)), size = 0.1) +
-  facet_grid(variable ~., labeller= ylab_label)   
-ggsave(filename= "Output//Figs/WTC_SoilTmpDepths.pdf", plot = TempDifDep, width = 6, height = 8)  
+source("R//Figs.R")
 
