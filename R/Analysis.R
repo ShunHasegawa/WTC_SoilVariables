8a12abe95444ab9455086031d694adf59dbcd96c
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-26 13:58:51 +1000

    daily mean is removed and daily summary is created instead

--- R/Analysis.R
+++ R/Analysis.R
@@ -68,45 +68,30 @@ PltAllSoil(data = DayCorrectSoil, filename= "Output/Figs/AllSoilVar_updated.pdf"
 ###########################
 #Daily Chamber & Trt mean #
 ###########################
-soilmlt <- melt(CorrectSoil, id = c("DateTime", "Date", "RECORD", "Source", "Chamber", "temp"), na.rm = TRUE)
-soilrang <- cast(soilmlt, Date + Chamber + temp ~ variable, range, na.rm = TRUE)
-
-smryDF <- function(){
-  Mean <- mean(value, na.rm = TRUE)
-  a <- range(value, na.rm = TRUE)
-  return(Mean = Mean, Min = a[1], Max = a[2])
-}
-
-test <- ddply(soilmlt[c(1:1000000),], .(Date, Chamber, temp, variable), smryDF, .progress = "text")
-head(test)
-
-
-tst <- soilmlt %.% 
-  group_by(Date, Chamber, temp, variable) %.%
-  summarise(Mean = mean(value, na.rm = TRUE),
-            Min = min(value, na.rm = TRUE),
-            Max = max(value, na.rm = TRUE))
-
-
-tst <- soilmlt %.% 
-  group_by(Date, Chamber, temp, variable) %.%
-  summarise(smryDF)
-
-
-head(tst)
-# 
-
-
-Mhead(soilrang)
-?cast
-head(soilmlt)
-
-
 # mean of SoilTemp10
 soilRowMean <- cbind(CorrectSoil[ ,-grep("SoilTemp10", names(CorrectSoil))], 
                      'SoilTemp10' = rowMeans(CorrectSoil[ ,grep("SoilTemp10", names(CorrectSoil))], na.rm = TRUE))
 
 summary(soilRowMean)
+
+soilmlt <- melt(soilRowMean, id = c("DateTime", "Date", "RECORD", "Source", "Chamber", "temp"), na.rm = TRUE)
+soilChmSmry <- soilmlt %.% 
+  group_by(Date, Chamber, temp, variable) %.%
+  summarise(Mean = mean(value, na.rm = TRUE),
+            Min = min(value, na.rm = TRUE),
+            Max = max(value, na.rm = TRUE))
+save(soilChmSmry, file = "Output/Data/WTC_soilMoistTemp_Chamber_DailySummary.RData")
+
+soilTrtSmry <- ddply(soilChmSmry, .(Date, temp, variable), function(x) colMeans(x[,c("Mean", "Min", "Max")], na.rm = TRUE))
+save(soilTrtSmry, file = "Output/Data/WTC_soilMoistTemp_TempTrt_DailySummary.RData")
+
+
+
+
+
+
+
+
 # Daily mean
 DayChmMean <- ddply(soilRowMean, .(Date, Chamber, temp), colSmryDF)
   # infinit was produced as there some factors which have only NA values
