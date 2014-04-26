7bde886cf3a8baea3110a509e236a737c88fb8dc
ShunHasegawa <S.Hasegawa@uws.edu.au>
2014-04-26 16:59:12 +1000

    line size for multiple-soil-temp plots are made smaller

--- R/Analysis.R
+++ R/Analysis.R
@@ -95,8 +95,9 @@ theme_set(theme_bw())
 # Moisture #
 ############
 # surface moisture
-PltMoist(data = subset(soilTrtSmry, variable == "SoilVW_5_25"), 
-                 ylab = "Soil moisture at 5-25 cm\n(% of volumetric water content)")
+TopMoist <- PltMoist(data = subset(soilTrtSmry, variable == "SoilVW_5_25"),
+                     ylab = "Soil moisture at 5-25 cm\n(% of volumetric water content)")
+ggsave(filename= "Output//Figs/WTC_SoilMoisture5_25cm.pdf", plot = TopMoist, width = 6, height = 3)
 
 # moisture at different depths
 ylabs <- list(
@@ -104,16 +105,20 @@ ylabs <- list(
   'SoilVW_30_50' = "30-50 cm",
   'SoilVW_HL' = "HL"
 )
-PltMoist(data = soilTrtSmry[grep("SoilVW", soilTrtSmry$variable), ], 
-         ylab = "Soil moisture\n(% of volumetric water content)") + 
+MoistDifDep <- PltMoist(data = soilTrtSmry[grep("SoilVW", soilTrtSmry$variable), ],
+                         ylab = "Soil moisture\n(% of volumetric water content)") + 
   facet_grid(variable ~., labeller= ylab_label)
+
+ggsave(filename= "Output//Figs/WTC_SoilMoistureDepths.pdf", plot = MoistDifDep, width = 6, height = 6)
+
 
 ###############
 # Temperature #
 ###############
 # surface temperature
-PltTemp(data = subset(soilTrtSmry, variable == "SoilTemp10"), 
-        ylab = expression(Soil~temperature~at~10~cm~(~degree~C)))
+ToTemp <- PltTemp(data = subset(soilTrtSmry, variable == "SoilTemp10"),
+                                ylab = expression(Soil~temperature~at~10~cm~(~degree~C)))
+ggsave(filename= "Output//Figs/WTC_SoilTemp10cm.pdf", plot = ToTemp, width = 6, height = 3)
 
 # stratified temperature
 ylabs <- list(
@@ -125,8 +130,8 @@ ylabs <- list(
   'Temp100' = "100 cm"
   )
 
-PltTemp(data = soilTrtSmry[grep("^Temp", soilTrtSmry$variable), ],
-        ylab = expression(Soil~temperature~(~degree~C))) +
+TempDifDep <- PltTemp(data = soilTrtSmry[grep("^Temp", soilTrtSmry$variable), ],
+                      ylab = expression(Soil~temperature~(~degree~C)), size = 0.1) +
   facet_grid(variable ~., labeller= ylab_label)   
-  
+ggsave(filename= "Output//Figs/WTC_SoilTmpDepths.pdf", plot = TempDifDep, width = 6, height = 8)  
 
